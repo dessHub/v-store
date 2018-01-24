9d3ec998165d4b5eb7a910ea9f92a28182c7402d
dessHub <deskip95@gmail.com>
2018-01-24 14:11:23 +0300

    initial commmits

---
+++ src/pages/Admin.vue
@@ -0,0 +1,21 @@
+
+<template>
+ <div>
+  <div class="title">
+   <h2>{{ msg }} </h2>
+  </div>
+ </div>
+</template>
+
+<script>
+ 
+ export default {
+   name: "home",
+   data() {
+     return {
+      msg: "Welcome to v-shop admin"
+     }
+   },
+ }
+
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,21 @@
+
+<template>
+ <div>
+   <div class="title">
+    <h2>{{ msg }}</h2>
+   </div>
+ </div>
+</template>
+
+<script>
+
+export default {
+   name: 'home',
+   data() {
+     return {
+      msg: "Hello , Welcome to my v-shop"
+   }
+   }
+}
+
+</script>
